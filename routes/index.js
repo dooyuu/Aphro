5918d1b45279bf38abc849aca06e6d4ed4e03c24
alayii <atsanmu@gmail.com>
2014-12-13 00:59:58 +0800

    添加semantic ui, 注释路由

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
+// 所有的路由写在这里
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+// 首页，render(arg1, arg2)
+// arg1: 渲染使用的文件名，views 目录中,
+// arg2(Object): 向前端模板传递的变量
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
